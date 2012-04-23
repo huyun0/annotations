040e1722aeeb14288b779b6ee0c4858179a4da22
Xavier Butty <buttyxavier@gmail.com>
2012-04-23 14:20:17 +0200

    Little modifications in the folders structure.

--- js/interfaces/player_adapter.js
+++ js/interfaces/player_adapter.js
@@ -14,7 +14,7 @@ define(function(){
      * @enum {number}
      */
     PlayerAdapter.STATUS = {
-            INITIALISING: 'initialising',
+            INITIALIZING: 'initializing',
             LOADING: 'loading',
             PLAYING: 'playing',
             PAUSED: 'paused',
---
+++ js/libs/require/config/test.js
@@ -0,0 +1,22 @@
+// Configuration for require.js
+require.config({
+    baseUrl: "../js",
+    paths: {
+        'root': 'libs',
+        'backbone':'libs/backbone/backbone-min',
+        'loader': 'libs/backbone/loader',
+        'localstorage': 'libs/backbone/backbone.localStorage-min',
+        'jquery': 'libs/jquery-1.7.2.min',
+        'interfaces': 'interfaces',
+        'order':'libs/require/order',
+        'underscore': 'libs/underscore-min',
+	'tests':'libs/tests',
+        'order':'libs/require/order',
+	'domReady':'libs/require/domReady'
+    },
+    waitSeconds: 10
+});
+
+ 
+
+
