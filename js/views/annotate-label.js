851751010d25d1ff596fe80714ba014302479869
Xavier Butty <xavier@entwinemedia.com>
2013-03-06 16:42:47 +0100

    Close #250

--- js/views/annotate-label.js
+++ js/views/annotate-label.js
@@ -116,7 +116,7 @@ define(["jquery",
               
               if (this.scaleValues) {
                   this.scaleValues = this.scaleValues.get("scaleValues");
-              }
+              } 
 
               if (_.contains(this.roles, annotationsTool.user.get("role"))) {
                   this.listenTo(annotationsTool.video, 'switchEditModus', this.onSwitchEditModus);
@@ -162,7 +162,7 @@ define(["jquery",
           annotate: function(){
               event.stopImmediatePropagation();
 
-              if (this.editModus) {
+              if (this.editModus || this.isScaleEnable) {
                 return;
               }
 
@@ -217,7 +217,6 @@ define(["jquery",
           },
 
           changeCategory: function (category) {
-
               var scale;
 
               if (category.scale) {
