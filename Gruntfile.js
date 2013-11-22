df740791051a57a6307b1be7e042b7b7fba02dd9
Xavier Butty <xavier@entwinemedia.com>
2013-11-22 10:15:52 +0100

    Add new task to workflow to ensure update of templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,40 +4,47 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    /** JSHint properties */
     jshintProperties: grunt.file.readJSON('build/jshint.json'),
 
-    currentTarget: "",
+    /** The current target file for the watch tasks */
+    currentWatchFile: "",
 
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'videoKIT.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
+    /** Local directory for the tests */
+    localDir: "www",
 
     jshint: {
-      all: '<%= currentTarget %>',
+      all: '<%= currentWatchFile %>',
       options: '<%= jshintProperties %>'
     },
 
     watch: {
       options: {
-        nospawn: true,
-        livereload: false
+        nospawn: true
       },
-
       // Watch Javascript file
       js: {
-        files: ["js/**/*.js"],
-        tasks: ['jshint', 'copy:local']
+        files: ["js/**/*.js", "templates/*.tmpl"],
+        //tasks: ['jshint', 'copy:local']
+        tasks: ['copy:local']
       },
       // Watch less file
       less: {
         files: ["style/**/*.less"],
-        tasks: ['less:annotation', 'copy:local']
+        tasks: ['less:annotation', 'copy:style']
+      },
+      // Watch templates files
+      templates: {
+        files: ["templates/*.tmpl"],
+        tasks: ['copy:local']
+      },
+      // Watch file on web server for live reload
+      www: {
+        options: {
+          livereload: true,
+          nospawn: true
+        },
+        files: ["www/**/*"]
       }
     },
 
@@ -48,6 +55,7 @@ module.exports = function(grunt) {
           syncImport: true,
           strictImports: true,
           concat: true,
+          compress: true,
           imports: {
             less: ['style/bootstrap/less/mixins.less', 'style/bootstrap/variables.less']
           }
@@ -63,8 +71,8 @@ module.exports = function(grunt) {
         files: [{
           flatten: false,
           expand: true,
-          src: '<%= currentTarget %>',
-          dest: '/Users/xavierbutty/Sites/DEMO_ANNOT',
+          src: '<%= currentWatchFile %>',
+          dest: 'www',
           filter: 'isFile'
         }]
       },
@@ -72,8 +80,17 @@ module.exports = function(grunt) {
         files: [{
           flatten: false,
           expand: true,
-          src: ['js/**/*', 'img/*', 'style/style.css', 'templates/*', 'resources/*', 'tests/**/*'],
+          src: ['js/**/*', 'img/**/*', 'style/**/*.png', 'style/**/*.css', 'templates/*', 'resources/*', 'tests/**/*'],
           dest: 'www',
+        }]
+      },
+      'style': {
+        files: [{
+          flatten: false,
+          expand: true,
+          src: 'style/style.css',
+          dest: 'www/',
+          filter: 'isFile'
         }]
       },
       'local-index': {
@@ -89,7 +106,7 @@ module.exports = function(grunt) {
 
     concurrent: {
       dev: {
-        tasks: ['watch:js', 'watch:less', 'connect'],
+        tasks: ['watch:js', 'watch:less', 'watch:www', 'connect'],
         options: {
           logConcurrentOutput: true
         }
@@ -109,14 +126,13 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-blanket-qunit');
+  grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('assemble-less');
   grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint:all', 'less-all', 'copy:local-all']);
@@ -124,10 +140,22 @@ module.exports = function(grunt) {
 
   // on watch events configure jshint:all to only run on changed file
   grunt.event.on('watch', function(action, filepath, target) {
-    grunt.log.writeln(target + ': ' + filepath + ' has ' + action + " ------ ");
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
 
-    grunt.config.set('currentTarget', [filepath]);
+    if (target == "js") {
+      var ext = filepath.split(".").pop();
+      grunt.log.writeln("Extension: " + ext);
 
+      switch (ext) {
+        case "js": 
+            grunt.task.run('jshint');
+            break;
+        case "tmpl":
 
+            break;
+      }
+    }
+
+    grunt.config.set('currentWatchFile', [filepath]);
   });
 };
