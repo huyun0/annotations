59d4e62352c853a54950eea25d4a522a5455d7b1
Xavier Butty <xavier@entwinemedia.com>
2015-07-07 15:31:51 +0200

    Version from 07.07.2015 with fixes for #418 and #421.

---
+++ templates/tracks-selection-item.js
@@ -0,0 +1,33 @@
+define(['handlebars'], function(Handlebars) {
+
+return Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n    <li class=\"list-group-item\">\n        <input type=\"checkbox\" value=\""
+    + escapeExpression(((stack1 = (depth0 && depth0.id)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\" ";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.visible), {hash:{},inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "> "
+    + escapeExpression(((stack1 = (depth0 && depth0.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\n    </li>\n";
+  return buffer;
+  }
+function program2(depth0,data) {
+  
+  
+  return "checked";
+  }
+
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.tracks), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n";
+  return buffer;
+  })
+
+});
